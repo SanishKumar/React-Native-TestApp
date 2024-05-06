73e53a4830aed1d8a1606bb0a8b53852ba5c0c52
SanishKumar <18sanishkumar@gmail.com>
2024-05-06 22:27:49 +0530

    layout

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,7 +1,12 @@
-import { StyleSheet, Text, View } from "react-native";
-import { Slot, Stack } from "expo-router";
-import React from "react";
+import { useEffect } from "react";
 import { useFonts } from "expo-font";
+import "react-native-url-polyfill/auto";
+import { SplashScreen, Stack } from "expo-router";
+
+import GlobalProvider from "../context/GlobalProvider";
+
+// Prevent the splash screen from auto-hiding before asset loading is complete.
+SplashScreen.preventAutoHideAsync();
 
 const RootLayout = () => {
   const [fontsLoaded, error] = useFonts({
@@ -24,24 +29,24 @@ const RootLayout = () => {
     }
   }, [fontsLoaded, error]);
 
+  if (!fontsLoaded) {
+    return null;
+  }
+
   if (!fontsLoaded && !error) {
     return null;
   }
 
   return (
-    <Stack>
-      <Stack.Screen name="index" options={{ headerShown: false }} />
-    </Stack>
+    <GlobalProvider>
+      <Stack>
+        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        <Stack.Screen name="(auth)" options={{ headerShown: false }} />
+        <Stack.Screen name="index" options={{ headerShown: false }} />
+        <Stack.Screen name="search/[query]" options={{ headerShown: false }} />
+      </Stack>
+    </GlobalProvider>
   );
 };
 
 export default RootLayout;
-
-// const styles = StyleSheet.create({
-//   container: {
-//     display: 'flex',
-//     flex: 1,
-//     alignItems: 'center',
-//     justifyContent: 'center',
-//   }
-// })
