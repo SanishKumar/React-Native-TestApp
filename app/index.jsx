116a5f9eb6e77de8970beb87c46895fc9ff2fa9b
SanishKumar <18sanishkumar@gmail.com>
2024-05-09 21:50:38 +0530

    Revert "Tabs Working"
    This reverts commit b49ecee024dfc6ae9794147fed44efbd71d3569d.

--- app/index.jsx
+++ app/index.jsx
@@ -1,15 +1,20 @@
-import { Link } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
-import { Text, View } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-3xl font-pblack">Aura!</Text>
+    <View style={styles.container}>
+      <Text>Hello Sanish!</Text>
       <StatusBar style="auto" />
-      <Link href="/home" style={{ color: 'blue' }}>Go to Home</Link>
     </View>
   );
 };
 
-
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
