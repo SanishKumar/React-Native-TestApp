c161f2144406036569aee27f203f3df7865233a5
SanishKumar <18sanishkumar@gmail.com>
2024-07-03 22:32:54 +0530

    Tabs Done

--- app/index.jsx
+++ app/index.jsx
@@ -7,7 +7,7 @@ export default function App() {
     <View className="flex-1 items-center justify-center bg-white">
       <Text className="text-3xl font-black">AORA!</Text>
       <StatusBar style="auto" />
-      <Link href="/">Go To</Link>
+      <Link href="/home">Go To Home</Link>
     </View>
   );
 }
